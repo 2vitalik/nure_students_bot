cc48390221e71ecd108f4d103c220be0f185e124
2vitalik <2vitalik@gmail.com>
2023-04-16 21:01:35 +0300

    tg: Save also sent message objects

--- bot/utils/tg_utils.py
+++ bot/utils/tg_utils.py
@@ -3,18 +3,22 @@ from datetime import datetime
 
 import shared_utils.api.telegram.telegram_utils as tg
 from shared_utils.io.io import append
-from shared_utils.io.json import json_dumps
+from shared_utils.io.json import json_dumps, json_dump
 from telegram.error import TimedOut, RetryAfter
 
 import conf
 
 
-def tg_send(bot, chat_id, text, keyboard=None, buttons=None, silent=False):
+def get_filename(chat_id, text, folder):
     now = datetime.now()
     month, dt = now.strftime('%Y-%m'), now.strftime('%Y-%m-%d %H-%M-%S')
     hash_value = str(abs(hash(text)))[:7]
-    filename = f'{conf.data_path}/messages/output/{month}/{chat_id}/' \
-               f'{dt} - {chat_id} - {hash_value}.json'
+    return f'{conf.data_path}/messages/{folder}/{month}/{chat_id}/' \
+           f'{dt} - {chat_id} - {hash_value}.json'
+
+
+def tg_send(bot, chat_id, text, keyboard=None, buttons=None, silent=False):
+    out_filename = get_filename(chat_id, text, 'output')
 
     line = '-' * 79
     output = f'''
@@ -26,11 +30,15 @@ def tg_send(bot, chat_id, text, keyboard=None, buttons=None, silent=False):
 {json_dumps(buttons)}
 {line}
     '''.strip()
-    append(filename, output)
+    append(out_filename, output)
 
     try:
         message = tg.send(bot, chat_id, text, keyboard, buttons, silent)
-        append(filename, 'SENT')
+        append(out_filename, 'SENT')
+
+        msg_filename = get_filename(chat_id, text, 'sent')
+        json_dump(msg_filename, message.to_dict())
+
         return message
 
     except (TimedOut, RetryAfter) as e:
@@ -42,5 +50,5 @@ def tg_send(bot, chat_id, text, keyboard=None, buttons=None, silent=False):
         time.sleep(60)
 
         message = tg_send(bot, chat_id, text, keyboard, buttons, silent)
-        append(filename, 'DELAYED SENT')
+        append(out_filename, 'DELAYED SENT')
         return message
