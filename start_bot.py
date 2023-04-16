729e9965e163b8e3c979781fe376093475a84714
2vitalik <2vitalik@gmail.com>
2023-04-16 20:13:06 +0300

    bot: Minor refactoring

--- start_bot.py
+++ start_bot.py
@@ -21,29 +21,39 @@ def start_bot():
     shared_conf.slack_hooks = conf.slack_hooks
 
     bot = Bot(conf.telegram_token)
-    bot.send_message(chat_id=conf.telegram_admin, text='💬 Starting the bot...')
+    bot.send_message(conf.telegram_admin, '💬 Starting the bot...')
 
     text_handler = TextHandler()
     members_handler = MembersHandler()
 
     updater = Updater(token=conf.telegram_token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(PollHandler(process_poll))
-    dispatcher.add_handler(PollAnswerHandler(process_poll_answer))
-    dispatcher.add_handler(CommandHandler('text', text_handler.text))
-    dispatcher.add_handler(CommandHandler('vote', text_handler.vote))
-    dispatcher.add_handler(CommandHandler('register', register))
-    dispatcher.add_handler(MessageHandler(Filters.text, text_handler.default))
-    dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members,
-                                          members_handler.default))
-    dispatcher.add_handler(MessageHandler(Filters.status_update.left_chat_member,
-                                          members_handler.default))
-    dispatcher.add_handler(CallbackQueryHandler(callbacks))
+    d = updater.dispatcher
+
+    # polls:
+    d.add_handler(PollHandler(process_poll))
+    d.add_handler(PollAnswerHandler(process_poll_answer))
+
+    # commands:
+    d.add_handler(CommandHandler('text', text_handler.text))
+    d.add_handler(CommandHandler('vote', text_handler.vote))
+    d.add_handler(CommandHandler('register', register))
+
+    # messages:
+    d.add_handler(MessageHandler(Filters.text, text_handler.default))
+
+    # members:
+    d.add_handler(MessageHandler(Filters.status_update.new_chat_members,
+                                 members_handler.default))
+    d.add_handler(MessageHandler(Filters.status_update.left_chat_member,
+                                 members_handler.default))
+
+    # callbacks:
+    d.add_handler(CallbackQueryHandler(callbacks))
 
     updater.start_polling()
     print('Bot has successfully started.')
     updater.idle()
-    print('Bot has finished.')
+    print('Bot has stopped.')
 
 
 if __name__ == '__main__':
