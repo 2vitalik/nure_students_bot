120548f763226b30d9565d38ab977cabb4c1462a
Vitaliy Lyapota <2vitalik@gmail.com>
2023-08-24 01:32:26 +0300

    members: Dump to separate json files

--- bot/members.py
+++ bot/members.py
@@ -1,6 +1,7 @@
 from os.path import join
 
 from shared_utils.io.io import append
+from shared_utils.io.json import json_dump
 
 import conf
 from bot.utils.errors import errors
@@ -9,3 +10,7 @@ from bot.utils.errors import errors
 @errors('members')
 def members(update, context):
     append(join(conf.data_path, 'members.txt'), update.to_json())
+
+    update_id = update['update_id']
+    json_dump(f'{conf.data_path}/members/json/{update_id}.json',
+              update.to_dict())
