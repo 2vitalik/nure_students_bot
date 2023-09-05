d55559f78e33dc40ed1f80ac0292b5327cf83c40
Vitaliy Lyapota <2vitalik@gmail.com>
2023-09-05 17:08:16 +0300

    pull: Split big functions into smaller ones

--- cron/pull_from_coda.py
+++ cron/pull_from_coda.py
@@ -7,7 +7,21 @@ from tools.coda import coda_doc
 from tools.data import sorted_by_keys
 
 
-def pull_from_coda():
+def pull_from_coda(table_slug, coda_table):
+    rows = {}
+    for row in coda_table.all():
+        row_id = row['@id']
+        columns = coda_table.overridden.values()
+        rows[row_id] = {
+            column: row[column]
+            for column in columns
+        }
+
+    filename = conf.coda_json_path / 'tables' / f'{table_slug}.json'
+    json_dump(filename, sorted_by_keys(rows))
+
+
+def pull_all_from_coda():
     tables = {
         'teachers': coda_doc.Teachers,
         'students': coda_doc.Students,
@@ -20,22 +34,34 @@ def pull_from_coda():
         'chats_other': coda_doc.TelegramOtherChats,
     }
 
-    for table_slug, table in tables.items():
-        rows = {}
-        for row in table.all():
-            row_id = row['@id']
-            columns = table.overridden.values()
-            rows[row_id] = {
-                column: row[column]
-                for column in columns
-            }
-
-        filename = conf.coda_json_path / 'tables' / f'{table_slug}.json'
-        json_dump(filename, sorted_by_keys(rows))
+    for table_slug, coda_table in tables.items():
+        pull_from_coda(table_slug, coda_table)
 
 
-def update_tg_jsons():
-    files = [
+def update_tg_json(filename):
+    coda_table = json_load(conf.coda_json_path / 'tables' / filename)
+
+    tg_json = {}
+    for row_id, coda_row in coda_table.items():
+        tg_id = coda_row['telegram_id']
+        tg_id_str = str(tg_id)
+        if not tg_id:
+            continue
+        if tg_id in tg_json:
+            raise Exception(f'Error: Duplicated `tg_id`: {tg_id} '
+                            f'for the file: {filename}')
+
+        tg_data = {'row_id': row_id, **coda_row}
+        del tg_data['telegram_id']
+
+        tg_json[tg_id_str] = tg_data
+
+    json_dump(conf.coda_json_path / 'tg_jsons' / filename,
+              sorted_by_keys(tg_json))
+
+
+def update_all_tg_jsons():
+    filenames = [
         'teachers.json',
         'students.json',
         'tg_users.json',
@@ -45,26 +71,8 @@ def update_tg_jsons():
         'chats_other.json',
     ]
 
-    for file in files:
-        coda_table = json_load(conf.coda_json_path / 'tables' / file)
-
-        tg_json = {}
-        for row_id, coda_row in coda_table.items():
-            tg_id = coda_row['telegram_id']
-            tg_id_str = str(tg_id)
-            if not tg_id:
-                continue
-            if tg_id in tg_json:
-                raise Exception(f'Error: Duplicated `tg_id`: {tg_id} '
-                                f'for the file: {file}')
-
-            tg_data = {'row_id': row_id, **coda_row}
-            del tg_data['telegram_id']
-
-            tg_json[tg_id_str] = tg_data
-
-        filename = conf.coda_json_path / 'tg_jsons' / file
-        json_dump(filename, sorted_by_keys(tg_json))
+    for filename in filenames:
+        update_tg_json(filename)
 
 
 if __name__ == '__main__':
