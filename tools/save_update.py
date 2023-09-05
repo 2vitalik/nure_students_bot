14eea169720e75da48190d9f0f92a2b187c25a89
Vitaliy Lyapota <2vitalik@gmail.com>
2023-09-06 00:40:56 +0300

    save_update: Separate date and time with `__`

--- tools/save_update.py
+++ tools/save_update.py
@@ -9,7 +9,7 @@ def save_update(func):
     def wrapped(update, context):
         update_id = str(update['update_id'])
         sub_folder = update_id[:-3]
-        now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        now = datetime.now().strftime('%Y-%m-%d__%H-%M-%S')
 
         filename = f'{conf.data_path}/updates/queue.todo/{sub_folder}/' \
                    f'{update_id}__{now}.json'
