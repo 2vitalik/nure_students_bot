33de79544598de65c5d57665967a5a297e866350
Vitaliy Lyapota <2vitalik@gmail.com>
2023-09-08 17:00:49 +0300

    slack: Add exceptions detailed info sending

--- tools/errors.py
+++ tools/errors.py
@@ -1,15 +1,20 @@
 import conf
+from tools.slack import slack_exception
 from tools.tg import tg_send
 
 
 def errors(slug):
     def decorator(func):
         def wrapped(*args, **kwargs):
+            from shared_utils.conf import conf as shared_conf
+            shared_conf.slack_hooks = conf.slack_hooks
+
             try:
                 return func(*args, **kwargs)
             except Exception as e:
                 message = f'<code>{slug}</code>\n' \
                           f'⚠️ <b>{type(e).__name__}</b>: {str(e)}'
+                slack_exception(slug, e)
                 tg_send(conf.telegram_error, message)
                 raise
         return wrapped
