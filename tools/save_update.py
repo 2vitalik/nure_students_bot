5e2759b67e20f89ff32d46271cd6cf57391fec54
Vitaliy Lyapota <2vitalik@gmail.com>
2025-10-01 04:20:45 +0300

    db: Save tg_updated on each update

--- tools/save_update.py
+++ tools/save_update.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from shared_utils.io.json import json_dump
 
 import conf
+from mongo.db import db
 
 
 def save_update(func):
@@ -15,6 +16,7 @@ def save_update(func):
                    f'{update_id}__{now}.json'
 
         json_dump(filename, update.to_dict())
+        db.tg_updates.add(update.to_dict())
 
         return func(update, context)
     return wrapped
