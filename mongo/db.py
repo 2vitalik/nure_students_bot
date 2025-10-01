3efec0c2384e5b5e5a35b3a24531bcb780500481
Vitaliy Lyapota <2vitalik@gmail.com>
2025-10-01 04:21:16 +0300

    db: Add poll collections

--- mongo/db.py
+++ mongo/db.py
@@ -1,12 +1,20 @@
 from pymongo import MongoClient
 
 import conf
+from mongo.collections.tg_polls import TelegramPollsCollection
+from mongo.collections.tg_polls_answers import TelegramPollsAnswersCollection
+from mongo.collections.tg_polls_history import TelegramPollsHistoryCollection
+from mongo.collections.tg_updates import TelegramUpdatesCollection
 
 
 class MongoDB:
     def __init__(self):
         self.client = MongoClient(conf.mongo_cluster)
         self.database = self.client['nure_students']
+        self.tg_polls = TelegramPollsCollection(self)
+        self.tg_polls_history = TelegramPollsHistoryCollection(self)
+        self.tg_polls_answers = TelegramPollsAnswersCollection(self)
+        self.tg_updates = TelegramUpdatesCollection(self)
 
 
 db = MongoDB()
