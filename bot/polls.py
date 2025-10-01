11e62dda3af6672f0b4edf003b992075bf8d64e5
Vitaliy Lyapota <2vitalik@gmail.com>
2025-10-01 04:28:39 +0300

    db: Save poll data on related events

--- bot/polls.py
+++ bot/polls.py
@@ -4,6 +4,7 @@ from shared_utils.io.io import append
 from shared_utils.io.json import json_dump, json_dumps
 
 import conf
+from mongo.db import db
 from tools.errors import errors
 from tools.save_update import save_update
 
@@ -17,14 +18,18 @@ def get_poll_path(poll_id):
 def process_poll(update, context):
     poll = update.poll
     path = get_poll_path(poll.id)
+    data = poll.to_dict()
 
     # print(json_dumps(poll.to_dict()))
     # print()
 
     dt = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 
-    json_dump(f'{path}/history/{dt}.json', poll.to_dict())
-    json_dump(f'{path}/latest.json', poll.to_dict())
+    json_dump(f'{path}/history/{dt}.json', data)
+    json_dump(f'{path}/latest.json', data)
+
+    db.tg_polls_history.add(data)
+    db.tg_polls.update(poll.id, data)
 
 
 @errors('process_poll_answer')
@@ -42,3 +47,5 @@ def process_poll_answer(update, context):
 
     json_dump(f'{path}/answers/{dt} - {user} - {options}.json', poll.to_dict())
     append(f'{path}/answers.txt', f'[{dt}]: {user}: {options}')
+
+    db.tg_polls_answers.add(poll.to_dict())
